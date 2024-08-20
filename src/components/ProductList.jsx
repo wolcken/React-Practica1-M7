bc22ed924eca292e0c2b9bdd589f8803b463f900
Menfhis <alfredoxd00@hotmail.com>
2024-08-20 14:53:22 -0400

    Add Styles

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,19 +1,13 @@
 import React from 'react';
+import ProductCard from '../components/ProductCard';
 import '../styles/ProductList.css';
 
 const ProductList = ({ products }) => {
-    const productItems = products.map((product, index) => (
-        <div key={index} className="product-card">
-            <h3>{product.name}</h3>
-            <p>Category: {product.category}</p>
-            <p>Price: {product.price}</p>
-            <p>Stocked: {product.stocked ? "Yes" : "No"}</p>
-        </div>
-    ));
-
     return (
         <div className="product-list">
-            {productItems.length > 0 ? productItems : <p>No products found</p>}
+            {products.map(product => (
+                <ProductCard key={product.name} product={product} />
+            ))}
         </div>
     );
 };
